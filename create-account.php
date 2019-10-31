851a08770f83ed56c0d543a1ebb1b3927eefa067
Ashish Jaiswar <49193978+AshishJaiswar@users.noreply.github.com>
2019-10-31 15:40:39 +0530

    Index page almost completed

--- create-account.php
+++ create-account.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include('classes/DB.php');
@@ -58,64 +57,3 @@ if (!DB::query('SELECT username FROM users WHERE username=:username', array(':us
 } else {
       echo "Username taken.<br>";
 }
-=======
-<?php
-
-include('classes/DB.php');
-include('classes/Mail.php');
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-
-if (empty($username) && empty($password) && empty($email)) {
-      echo "All fields are required";
-      exit();
-} else {
-      if (empty($username)) {
-            echo "Please enter a username";
-            exit();
-      } else if (empty($password)) {
-            echo "Please enter a password";
-            exit();
-      } else if (empty($email)) {
-            echo "Please enter a email";
-            exit();
-      } else {
-            echo "";
-      }
-}
-if (!DB::query('SELECT username FROM users WHERE username=:username', array(':username' => $username))) {
-
-      if (strlen($username) >= 5 && strlen($username) <= 32) {
-
-            if (preg_match("/[a-zA-Z0-9_]+/", $username)) {
-
-                  if (strlen($password) >= 6 && strlen($password) <= 60) {
-
-                        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-                              if (!DB::query('SELECT email FROM users WHERE email=:email', array(':email' => $email))) {
-
-                                    DB::query('INSERT INTO users VALUES(\'\',:username,:password,:email,\'0\', \'\')', array(':username' => $username, ':password' => password_hash($password, PASSWORD_BCRYPT), ':email' => $email));
-                                    Mail::sendMail('Welcome to Friesta', 'Your Account has been created sucessfully.', $email);
-                                    echo "Sucess";
-                              } else {
-                                    echo 'User Already Registered.<br>';
-                              }
-                        } else {
-                              echo "Invalid email.<br>";
-                        }
-                  } else {
-                        echo "Password must be greater than 6 and less than 60.<br>";
-                  }
-            } else {
-                  echo "Invalid username.";
-            }
-      } else {
-            echo "Username must be greater than 5 and less than 32.<br>";
-      }
-} else {
-      echo "Username taken.<br>";
-}
->>>>>>> 14226c79fce92bcb97faa12b3af6bbe784836f0b
--- forgot-password.php
+++ forgot-password.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include('classes/DB.php');
 include('./classes/Mail.php');
@@ -21,28 +20,4 @@ if (isset($_POST['resetpassword'])) {
     <input type="submit" value="Reset Password" name="resetpassword">
 
 
-=======
-<?php
-include('classes/DB.php');
-include('./classes/Mail.php');
-
-if (isset($_POST['resetpassword'])) {
-    $cstrong = True;
-    $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
-    $email = $_POST['email'];
-    $user_id = DB::query('SELECT id FROM users WHERE email=:email', array(':email' => $email))[0]['id'];
-    DB::query('INSERT INTO password_tokens VALUES (\'\',:token, :user_id)', array(':token' => sha1($token), ':user_id' => $user_id));
-    Mail::sendMail('Forgot Password!', "Reset password ~ <br/><a href='http://localhost:8081/Friesta/change-password.php?token=$token'>http://localhost:8081/Friesta/change-password.php?token=$token</a>", $email);
-    echo "Email Sent";
-}
-?>
-
-<h1>Forgot Password</h1>
-<form action='forgot-password.php' method="post">
-    <input type="email" name="email" placeholder="Enter Email....">
-    <p />
-    <input type="submit" value="Reset Password" name="resetpassword">
-
-
->>>>>>> 14226c79fce92bcb97faa12b3af6bbe784836f0b
 </form>
